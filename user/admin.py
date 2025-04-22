1b41dbbf045e0bcbe2de31091df016092139135a
Windings-Lab <janton20001@gmail.com>
2025-04-22 16:52:30 +0300

    feat(user): implement email-based authentication
    * Replace username with email as the unique identifier
    * Add custom UserManager with creation methods for users and superusers
    * Update User model to remove username and set email as USERNAME_FIELD
    * Customize UserAdmin in Django admin to support email-based authentication

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,44 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin.decorators import register
+from django.contrib.auth import admin
+from django.utils.translation import gettext_lazy as _
+
 from .models import User
 
-admin.site.register(User, UserAdmin)
+
+@register(User)
+class UserAdmin(admin.UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "usable_password",
+                    "password1",
+                    "password2"
+                ),
+            },
+        ),
+    )
+
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
